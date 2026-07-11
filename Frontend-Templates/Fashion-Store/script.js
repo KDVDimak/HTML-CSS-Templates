dc368641057e8e92a5263c63d7e02d3057329609
Dmitry Kryvonos <162099821+KDVDimak@users.noreply.github.com>
2026-07-11 20:28:52 +0200

    Refactor script.js for improved

--- Frontend-Templates/Fashion-Store/script.js
+++ Frontend-Templates/Fashion-Store/script.js
@@ -1,1 +1,23 @@
-const b=document.querySelector('.menu');const n=document.querySelector('nav');b.onclick=()=>n.classList.toggle('open');const o=new IntersectionObserver(e=>e.forEach(x=>x.isIntersecting&&x.target.classList.add('show')),{threshold:.15});document.querySelectorAll('.reveal').forEach(x=>o.observe(x));
+const b = document.querySelector(".menu");
+const n = document.querySelector("nav");
+
+b.onclick = () => {
+    n.classList.toggle("open");
+};
+
+const o = new IntersectionObserver(
+    (e) => {
+        e.forEach((x) => {
+            if (x.isIntersecting) {
+                x.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+document.querySelectorAll(".reveal").forEach((x) => {
+    o.observe(x);
+});
